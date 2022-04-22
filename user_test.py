4169e1abf186d3022ede07a18e04c102a3b5808c
Vanili <katevanili98@gmail.com>
2022-04-22 13:32:34 +0300

    Added the method that cleans up after each test run

--- user_test.py
+++ user_test.py
@@ -13,8 +13,27 @@ class TestUser (unittest.TestCase):
     '''
 
 def setUp(self):
+
     '''
         Set up method to run before each test cases.
     '''
 
     self.new_user = User("Kate Vanili", "Same#3030") # create user detail objects
+
+def test_init(self):
+
+    '''
+        test_init test case to test if the object is initialized properly
+    '''
+
+    self.assertEqual(self.new_user.username,"Kate Vanili")
+    self.assertEqual(self.new_user.password,"Same#3030")
+
+def tearDown(self):
+
+    '''
+            tearDown method that does clean up after each test case has run.
+    '''
+
+    User.user_accounts = []
+
