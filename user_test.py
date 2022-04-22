7f00b0e9b4db08b6bd3bddedebb072fa261d9471
Vanili <katevanili98@gmail.com>
2022-04-22 12:45:28 +0300

    Added a docstring to explain TestUser

--- user_test.py
+++ user_test.py
@@ -3,4 +3,11 @@ from user import User #imports the User class
 import pyperclip
 
 class TestUser (unittest.TestCase):
-    
+
+     '''
+    Test class that defines test cases for the User class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+
+    '''
